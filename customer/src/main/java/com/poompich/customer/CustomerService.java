b3ef056f86fb970333b66b5f6bac639cd5304697
poompichayout <poompichayout@gmail.com>
2023-03-13 17:43:12 +0700

    feat: using RestTemplate to connect services together

--- customer/src/main/java/com/poompich/customer/CustomerService.java
+++ customer/src/main/java/com/poompich/customer/CustomerService.java
@@ -2,12 +2,14 @@ package com.poompich.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -20,9 +22,18 @@ public class CustomerService {
 
         // TODO: check if email not taken
 
-        // TODO: check if fraudster
+        customerRepository.saveAndFlush(customer);
 
-        customerRepository.save(customer);
+        // TODO: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
 
         // TODO: send notification
     }
---
+++ customer/src/main/java/com/poompich/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.poompich.customer;
+
+public record FraudCheckResponse(boolean isFraudster) {
+}
